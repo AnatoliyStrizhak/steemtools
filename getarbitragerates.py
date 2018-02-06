9f7eebc3f3c57ccb02336e192e2dec9a384ce884
Anatoliy Strizhak <jrodger@yandex.ru>
2018-02-06 18:20:39 +0200

    Added Yobit exchange, scraper request to bypass cloudflare checks and bittrex check for temporary disabled coins

--- getarbitragerates.py
+++ getarbitragerates.py
@@ -1,14 +1,19 @@
 # This program calculates the ask and bid rates at different exchanges, and tells you the best option for arbitrage trading.
 # Author: Hamid Mushtaq
 import sys
-import json, requests
+import json, requests, cfscrape
 import datetime
+
+#its needed to bypass cloudflare checks
+scraper = cfscrape.create_scraper()
 
 rj_cryptopia = requests.get('https://www.cryptopia.co.nz/api/GetMarkets/BTC').json()['Data']
 rj_poloniex = requests.get('https://poloniex.com/public?command=returnTicker').json()
+rj_bittrex=requests.get('https://bittrex.com/api/v1.1/public/getcurrencies').json()['result']
+
 
 first_time = True
-EXCHANGES = ["Cryptopia", "Bittrex", "HitBTC", "Poloniex", "Binance"]
+EXCHANGES = ["Cryptopia", "Bittrex", "HitBTC", "Poloniex", "Binance", "YoBit"]
 
 # https://www.cryptopia.co.nz/Forum/Thread/255
 def getFromCryptopia(coin):
@@ -17,6 +22,7 @@ def getFromCryptopia(coin):
 			return (float(r['AskPrice']), float(r['BidPrice']))
 	return (0,0)
 	
+
 # https://poloniex.com/support/api/
 def getFromPoloniex(coin):
 	try:
@@ -25,22 +31,46 @@ def getFromPoloniex(coin):
 	except:
 		return (0,0)
 			
+
 # https://bittrex.com/home/api
 def getFromBittrex(coin):
-	try:
-		r = requests.get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-' + coin.lower()).json()['result'][0]
-		return (float(r['Ask']), float(r['Bid']))
-	except:
-		return (0,0)
-	
+    enabled="";
+    try:
+        for b in rj_bittrex:
+            if b['Currency']==coin.upper():
+                cenabled=b['IsActive']
+        if cenabled=='False':
+            return (0,0)
+        else:
+            r = requests.get('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-' + coin.lower()).json()['result'][0]
+            return (float(r['Ask']), float(r['Bid']))
+    except:
+        return (0,0)
+
+
 # https://api.hitbtc.com/?python#rest-api-reference
 def getFromHitBTC(coin):
 	try:
-		r = requests.get('https://api.hitbtc.com/api/2/public/ticker/' + coin.upper() + 'BTC').json()
-		return (float(r['ask']), float(r['bid']))
+		enabled=requests.get('https://api.hitbtc.com/api/2/public/currency/' + coin.upper()).json()
+		if enabled['payinEnabled']==false:
+		    return (0,0)
+		else:
+		    r = requests.get('https://api.hitbtc.com/api/2/public/ticker/' + coin.upper() + 'BTC').json()
+		    return (float(r['ask']), float(r['bid']))
 	except:
 		return (0,0)
 		
+
+
+# https://yobit.net/api/3/ticker/ltc_btc
+def getFromYobitBTC(coin):
+    try:
+        r = scraper.get("https://yobit.net/api/3/ticker/"+ coin.lower() + "_btc").json()[coin.lower() + "_btc"]
+        return (float(r['sell']), float(r['buy']))
+    except:
+        return (0,0)
+
+
 # https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md
 def getFromBinance(coin):
 	try:
@@ -68,7 +98,7 @@ def getBestRate(rasks, rbids):
 		
 def getAskAndBidStr(r):
 	if (r[0] == 0):
-		return 'Coin not found'
+		return 'Coin not found or disabled'
 	else:
 		return 'Ask=%g, Bid=%g' % (r[0], r[1])
 		
@@ -103,9 +133,10 @@ while True:
 	rhitbtc = getFromHitBTC(coin)
 	rpoloniex = getFromPoloniex(coin)
 	rbinance = getFromBinance(coin)
+	ryobit = getFromYobitBTC(coin)
 	
-	rasks = [rcryptopia[0], rbittrex[0], rhitbtc[0], rpoloniex[0], rbinance[0]]
-	rbids = [rcryptopia[1], rbittrex[1], rhitbtc[1], rpoloniex[1], rbinance[1]]
+	rasks = [rcryptopia[0], rbittrex[0], rhitbtc[0], rpoloniex[0], rbinance[0], ryobit[0]]
+	rbids = [rcryptopia[1], rbittrex[1], rhitbtc[1], rpoloniex[1], rbinance[1], ryobit[1]]
 	
 	now = datetime.datetime.now()
 	print(now.strftime("\nTime: %H:%M:%S"))
@@ -114,6 +145,7 @@ while True:
 	print('%10s: %s' % ('HitBTC', getAskAndBidStr(rhitbtc)))
 	print('%10s: %s' % ('Poloniex', getAskAndBidStr(rpoloniex)))
 	print('%10s: %s' % ('Binance', getAskAndBidStr(rbinance)))
+	print('%10s: %s' % ('Yobit', getAskAndBidStr(ryobit)))
 	
 	lowestAsk, highestBid, lowestAskIndex, highestBidIndex = getBestRate(rasks, rbids)
 	if (lowestAskIndex != -1) and (highestBidIndex != -1):
